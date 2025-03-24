0aa33fc22415481b6236359a2fb8d0572e8c4674
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2025-03-24 18:40:09 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -3,18 +3,18 @@ pip install -r requirements.txt --break-system-packages
 
 # Запуск скрипта генерации данных
 echo "Запуск data_creation.py..."
-python data_creation.py
+python3 data_creation.py
 
 # Запуск скрипта предобработки данных
 echo "Запуск model_processing.py..."
-python model_processing.py
+python3 model_processing.py
 
 # Запуск скрипта обучения модели
 echo "Запуск model_training.py..."
-python model_training.py
+python3 model_training.py
 
 # Запуск скрипта тестирования модели
 echo "Запуск model_testing.py.py..."
-python model_testing.py
+python3 model_testing.py
 
 echo "Pipeline завершен."
