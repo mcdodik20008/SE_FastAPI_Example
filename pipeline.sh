c7001568669d43cd7f400587bbf408252e26b590
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2025-03-24 18:31:06 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # Запуск скрипта генерации данных
 echo "Запуск data_creation.py..."
-python3 data_creation.py
+python data_creation.py
 
 # Запуск скрипта предобработки данных
 echo "Запуск model_processing.py..."
-python3 model_processing.py
+python model_processing.py
 
 # Запуск скрипта обучения модели
 echo "Запуск model_training.py..."
-python3 model_training.py
+python model_training.py
 
 # Запуск скрипта тестирования модели
 echo "Запуск model_testing.py.py..."
-python3 model_testing.py
+python model_testing.py
 
 echo "Pipeline завершен."
